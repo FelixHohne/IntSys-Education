43ea5474ee91db865599aa869ba457bf5b0ac201
sammy0703 <evelynwu0703@gmail.com>
2020-05-02 14:09:29 -0500

    some A3 updates

--- a3/neural_net.py
+++ a3/neural_net.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
 import numpy as np
 from data_loader import get_data_loaders
 from typing import List, Union, Tuple
@@ -19,23 +20,27 @@ class SimpleNeuralNetModel(nn.Module):
         # The first number represents the input size and the output would be
         # the last number, with the numbers in between representing the
         # hidden layer sizes
-
         self.linears = nn.ModuleList()
-        length = len(layer_sizes) 
+        self.softmax = nn.Softmax(dim = 1)
+        self.loss = nn.NLLLoss()
+        length = len(layer_sizes)
         for i in range (0, length -1): 
             self.linears.append(nn.Linear(layer_sizes[i], layer_sizes[i+1]))
     
-    def forward(x):
+    def forward(self, x):
         """forward generates the prediction for the input x.
         :param x: Input array of size (Batch,Input_layer_size)
         :type x: np.ndarray
         :return: The prediction of the model
         :rtype: np.ndarray
         """
-        for i in range (0,length): 
+        #TODO: Matrix mult then relu
+        m,n = x.size
+        for i in range (0,n-1):
             l = self.linears[i]
-            x = nn.RELU(x * l)
-        np.to_array(x)
+            x = nn.ReLU(x * l)
+        x = self.softmax(x * self.linears[n])
+        return np.to_array(x)
 
 
 class SimpleConvNetModel(nn.Module):
@@ -70,4 +75,22 @@ class SimpleConvNetModel(nn.Module):
 
 if __name__ == "__main__":
     neural_net = SimpleNeuralNetModel([400,200,10])
+    print(neural_net.linears)
+    train_load, val_load, test_load = get_data_loaders('train/train_samples.pkl',
+                                                       'train/correct_train_labels.pkl',
+                                                       'val/val_samples.pkl',
+                                                       'val/correct_val_labels.pkl',
+                                                       'test/test_samples.pkl',
+                                                       'test/correct_test_labels.pkl')
+
+    model = SimpleNeuralNetModel(layer_sizes=[784, 200, 200, 10])
+    optimizer = optim.Adam(params=model.parameters(), lr = 0.01)
+    model.train()
+    for t in range(100):
+        for batch_index, (input_t, y) in enumerate(train_load):
+            optimizer.zero_grad()
+            preds = model(input_t)
+            loss = model.loss(preds, y.view(1,len(y)))
+            loss.backward()
+            optimizer.step()
 
